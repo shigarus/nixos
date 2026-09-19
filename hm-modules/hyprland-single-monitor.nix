4793a468d46314836fc92d500adc35783c1b0045
Aleksandr Stovbyra <shiggarus@gmail.com>
2026-09-19 22:58:27 +0200

    fix hyprland disabling monitor

--- hm-modules/hyprland-single-monitor.nix
+++ hm-modules/hyprland-single-monitor.nix
@@ -4,21 +4,18 @@
     Unit = {
       Description = "Disables first monitor when adding second.";
     };
-    Install = {
-      WantedBy = [ "default.target" ];
-    };
     Service = {
       ExecStart = "${pkgs.writeShellScript "hyprland-single-monitor" ''
-        #!/run/current-system/sw/bin/bash
+        #!/bin/sh
 		check_monitors() {
 			# Count connected monitors using hyprctl JSON output
 			count=$(hyprctl monitors -j | jq length)
 			
 			# Adjust 'eDP-1' to your built-in monitor name
 			if [ "$count" -gt 1 ]; then
-				hyprctl keyword monitor "eDP-1, disable"
+				hyprctl dispatch 'hl.dsp.dpms({action="disable", monitor="eDP-1"})'
 			else
-				hyprctl keyword monitor "eDP-1, preferred, auto, 1"
+				hyprctl dispatch 'hl.dsp.dpms({action="enable", monitor="eDP-1"})'
 			fi
 		}
 
